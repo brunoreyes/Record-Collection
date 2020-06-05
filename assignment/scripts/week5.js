94569c9e1cf34fceb2759424d878e79decbe3d9f
Bruno Reyes <bruno619reyes@gmail.com>
2020-06-05 10:00:41 -0500

    Switched around property names in addToCollection

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,13 +1,24 @@
 console.log('js');
+//addToCollection
 let collection = [];
-function addToCollection(title = null,artist = null,year = null){
-    console.log('in addToCollection',title,artist,year);
-    let record = {recordTitle: title, recordArtist: artist, recordYear: year};
+function addToCollection(recordTitle = null,recordArtist = null,recordYear = null){
+    console.log('in addToCollection',recordTitle,recordArtist,recordYear);
+    let record = { title: recordTitle, artist: recordArtist, year: recordYear};
     collection.push(record);
     return record;
-}
+}// end addToCollection
 addToCollection(`Burn`,`Usher`, 2004);
 addToCollection(`Summertime Sadness`,`Lana Del Rey`, 2012);
-addToCollection(`Change Is Gonna Come`,`Sam Cooke`, 1964);
-console.log(`There are`,collection.length,`records in the collection:`,collection);
-
+addToCollection(`Change Is Gonna Come`, `Sam Cooke`, 1964);
+addToCollection(`Burn`,`Ellie Goulding`, 2013);
+console.log(`There are (expect 4)`,collection.length,`records in the collection:`,collection);
+//findRecord 
+//function findRecord(title) {
+//    console.log('in findRecord', title);
+//for( let)          return true;
+  //      }// end if statement
+ //     }// end for statement
+//        return false;
+ //   }// end isParkedHere
+//}
+//console.log('');
