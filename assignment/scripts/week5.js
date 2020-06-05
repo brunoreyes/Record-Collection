c8f585d9b02ec576e971ff3d7a127f94f0afba61
Bruno Reyes <bruno619reyes@gmail.com>
2020-06-05 09:24:52 -0500

    Changed CSS bkd img to body

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,1 +1,13 @@
 console.log('js');
+let collection = [];
+function addToCollection(title = null,artist = null,year = null){
+    console.log('in addToCollection',title,artist,year);
+    let record = {recordTitle: title, recordArtist: artist, recordYear: year};
+    collection.push(record);
+    return record;
+}
+addToCollection(`Burn`,`Usher`, 2004);
+addToCollection(`Summertime Sadness`,`Lana Del Rey`, 2012);
+addToCollection(`Change Is Gonna Come`,`Sam Cooke`, 1964);
+console.log(`There are`,collection.length,`records in the collection:`,collection);
+
