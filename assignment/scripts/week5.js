7cf52052bebea9cb52f88dcaa3d2eebef73dad33
Bruno Reyes <brunoreyes@Brunos-MacBook-Pro.local>
2020-06-06 22:08:18 -0500

    Edited findRecord Function

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -3,22 +3,29 @@ console.log('js');
 let collection = [];
 function addToCollection(recordTitle = null,recordArtist = null,recordYear = null){
     console.log('in addToCollection',recordTitle,recordArtist,recordYear);
-    let record = { title: recordTitle, artist: recordArtist, year: recordYear};
+    let record = { title: recordTitle, artist: recordArtist, year: recordYear };
     collection.push(record);
-    return record;
+    console.log(`-- Records in Collection: ${collection.length} --`)
+    return true;
 }// end addToCollection
 addToCollection(`Burn`,`Usher`, 2004);
 addToCollection(`Summertime Sadness`,`Lana Del Rey`, 2012);
 addToCollection(`Change Is Gonna Come`, `Sam Cooke`, 1964);
 addToCollection(`Burn`,`Ellie Goulding`, 2013);
-console.log(`There are (expect 4)`,collection.length,`records in the collection:`,collection);
+console.log(`There are (expect 4)`, collection.length, `records in the collection:`, collection);
 //findRecord 
-//function findRecord(title) {
-//    console.log('in findRecord', title);
-//for( let)          return true;
-  //      }// end if statement
- //     }// end for statement
-//        return false;
- //   }// end isParkedHere
-//}
-//console.log('');
+function findRecord(title){
+    console.log('in findRecord', title);
+    for (let i = 0; i < collection.length; i++){
+        for (let property in i) {
+            if (i.property === title) {
+                return `Index value of 
+                    $`
+                }
+            }
+}// end findRecord
+
+console.log(findRecord('Burn'));
+console.log(findRecord('Yeah'));
+console.log(findRecord('Sam Cooke'));
+
