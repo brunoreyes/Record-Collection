7222c72258667d8c00d037089c7242b6a1214c05
Bruno Reyes <brunoreyes@Brunos-MacBook-Pro.local>
2020-06-07 20:28:47 -0500

    Edited findRecord & allby with succesful test

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,31 +1,64 @@
-console.log('js');
-//addToCollection
+//console.log('js');
 let collection = [];
+
 function addToCollection(recordTitle = null,recordArtist = null,recordYear = null){
-    console.log('in addToCollection',recordTitle,recordArtist,recordYear);
     let record = { title: recordTitle, artist: recordArtist, year: recordYear };
     collection.push(record);
-    console.log(`-- Records in Collection: ${collection.length} --`)
+    console.log(`Records in Collection: ${collection.length} `)
     return true;
-}// end addToCollection
+}
+console.log(`--- Testing addToCollection() ---`)
 addToCollection(`Burn`,`Usher`, 2004);
 addToCollection(`Summertime Sadness`,`Lana Del Rey`, 2012);
 addToCollection(`Change Is Gonna Come`, `Sam Cooke`, 1964);
 addToCollection(`Burn`,`Ellie Goulding`, 2013);
-console.log(`There are (expect 4)`, collection.length, `records in the collection:`, collection);
-//findRecord 
-function findRecord(title){
-    console.log('in findRecord', title);
-    for (let i = 0; i < collection.length; i++){
-        for (let property in i) {
-            if (i.property === title) {
-                return `Index value of 
-                    $`
-                }
+console.log(`They're (expect 4)`, collection.length, `records in the collection:`, collection);
+
+function findRecord(recordTitle){
+    for(let i = 0; i < collection.length; i++){
+        if(collection[i].title === recordTitle){
+            return i;
+        }
+    } 
+    return false;
+}
+console.log(`--- Testing findRecord() ---`)
+console.log(`(expect 0)`,(findRecord('Burn')));
+console.log(`(expect false)`,(findRecord('Yeah')));
+console.log(`(expect false)`,(findRecord('Sam Cooke')));
+console.log(`(expect false)`,(findRecord(2004)));
+
+function allBy(recordArtist){
+    for(let i = 0; i < collection.length; i++){
+        if(collection[i].artist === recordArtist){
+            return recordArtist;
+        }
+    }
+    return false;
+}
+console.log(`--- Testing allBy() ---`)
+console.log(`(expect false)`,(allBy('Burn')));
+console.log(`(expect false)`,(allBy('Celine Dion')));
+console.log(`(expect Sam Cooke)`,(allBy('Sam Cooke')));
+console.log(`(expect false)`,(allBy(2004)));
+
+function search(recordTrait){
+    for(let i = 0; i < collection.length; i++){
+        for(let property in collection[i]){
+            let record = collection[i];
+            if(record[property] === recordTrait){
+                return `Index value of ${i}`;
             }
-}// end findRecord
-
-console.log(findRecord('Burn'));
-console.log(findRecord('Yeah'));
-console.log(findRecord('Sam Cooke'));
-
+            else if(record[property] === ' '){
+                return collection;
+            }
+        }
+    }
+    return false;
+}
+console.log(`--- Testing search() ---`)
+console.log(`(expect false)`,(search(' ')));
+console.log(`(expect false)`,(search('2012')));
+console.log(`(expect false)`,(search('Lana Del Rey')));
+console.log(`(expect Sam Cooke)`,(search('Change Is Gonna Come')));
+console.log(`(expect false)`,(search(1900)));
