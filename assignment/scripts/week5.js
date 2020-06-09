6b8e59346081989f145ef5c09d3b4ab7798371d6
Bruno Reyes <brunoreyes@Brunos-MacBook-Pro.local>
2020-06-09 00:49:51 -0500

    Modified search function, started dom goal

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,7 +1,22 @@
 //console.log('js');
 let collection = [];
+document.querySelector("#form").addEventListener("submit", function(event){
+    let title = document.querySelector("#title").value;
+    let artist = document.querySelector("#artist").value;
+    let year = document.querySelector("#year").value;
+    addToCollection(title, artist, year);
+    console.log(`baby`);
+    event.preventDefault();
+});
+//the function is called when the button is pressed
 
-function addToCollection(recordTitle = null,recordArtist = null,recordYear = null){
+function addRow(record){
+    let tbody = document.querySelector("#tbody")
+    
+}
+
+
+function addToCollection(recordTitle,recordArtist,recordYear){
     let record = { title: recordTitle, artist: recordArtist, year: recordYear };
     collection.push(record);
     console.log(`Records in Collection: ${collection.length} `)
@@ -9,11 +24,13 @@ function addToCollection(recordTitle = null,recordArtist = null,recordYear = nul
 }
 console.log(`--- Testing addToCollection() ---`)
 addToCollection(`Burn`,`Usher`, 2004);
-addToCollection(`OMG`,`Usher`, 2004);
+addToCollection(`OMG`,`Usher`, 2008);
 addToCollection(`Summertime Sadness`,`Lana Del Rey`, 2012);
 addToCollection(`Change Is Gonna Come`, `Sam Cooke`, 1964);
 addToCollection(`Burn`,`Ellie Goulding`, 2013);
-console.log(`They're (expect 4)`, collection.length, `records in the collection:`, collection);
+addToCollection(`Usher`,`Nobody`, 2020);
+addToCollection(`Usher`,`Somebody`, 2020);
+// console.log(`They're (expect 4)`, collection.length, `records in the collection:`, collection);
 
 function findRecord(recordTitle){
     for(let i = 0; i < collection.length; i++){
@@ -28,47 +45,76 @@ console.log(`(expect 0)`,(findRecord('Burn')));
 console.log(`(expect false)`,(findRecord('Yeah')));
 console.log(`(expect false)`,(findRecord('Sam Cooke')));
 console.log(`(expect false)`,(findRecord(2004)));
+//Change later.. test more than 1 true case, test 1 true value in beginning and 1 at end to see if it still works
 
+//We need to fix this statement
 function allBy(recordArtist){
-    let allByArtist = [];
+    let allByRecords = [];
     for(let i = 0; i < collection.length; i++){
         if(collection[i].artist === recordArtist){
-            allByArtist.push(allByArtist);
-            return allByArtist;
+            allByRecords.push(collection[i]);
+            // collection[i] is accessing record index, so 0 is Burn, 1 is OMG ..  
+            //Spend more time reading the question and understanding absolutely what you need! We didn't need false! 
         }
     }
-    return false;
+    return allByRecords;
 }
-
-
 console.log(`--- Testing allBy() ---`)
-console.log(`(expect false)`,(allBy('Burn')));
-console.log(`(expect false)`,(allBy('Celine Dion')));
-console.log(`(expect array of Usher Songs)`,(allBy('Sam Cooke')));
-console.log(`(expect false)`,(allBy(2004)));
-
-function search(recordTrait){
+console.log(`allBy('Burn') (expect an empty array)`,(allBy('Burn')));
+console.log(`allBy('Celine Dion') (expect an empty array)`,(allBy('Celine Dion')));
+console.log(`allBy('Usher') (expect 2 Usher record onjects returned in an array)` ,(allBy(`Usher`)));
+console.log(`allBy('2004') (expect an empty array)`,(allBy(2004)));
+console.log(`allBy('Ellie Goulding') (expect an index value of 3) `,(allBy(`Ellie Goulding`)));
+function allYear(recordYear){
+    let yearCollection = [];
     for(let i = 0; i < collection.length; i++){
-        for(let property in collection[i]){
-            let record = collection[i];
-            if(record[property] === recordTrait){
-                return `Index value of ${i}`;
-            }
-            else if(recordTrait === undefined || Object.keys(recordTrait).length === 0 ){
-                return collection;
-            }
-            else if(allBy()){
-                return recordArtist;
-            }
-            
+        if(collection[i].year === recordYear){
+            yearCollection.push(collection[i]);
         }
     }
-    return false;
+    return yearCollection;
 }
-console.log(`--- Testing search() ---`)
-console.log(`(expect collection)`,(search('')));
-console.log(`(expect collection)`,(search()));
+console.log(`expect 2 records)`,allYear(2020));
+console.log(`expect 1 record)`,allYear(2012));
+console.log(`expect 0 record)`,allYear(1900));
 
-console.log(`(expect false)`,(search('2012')));
-console.log(`(expect false)`,(search('Lana Del Rey')));
-console.log(`(expect Sam Cooke)`,(search('Change Is Gonna Come')));
+
+
+function search(recordObject){
+    let matchingRecords = [];
+    if(recordObject === undefined || Object.keys(recordObject).length === 0){
+        return collection;
+    }
+    for(let record of collection){
+        let matchingKeys = true;
+        for(let key of Object.keys(recordObject)){
+            if(recordObject[key] !== record[key]){
+                matchingKeys = false;
+                break;
+            }
+        }
+        if(matchingKeys){
+            matchingRecords.push(record);
+        }
+    }
+    return matchingRecords;//`No matches were found, please input an object with a year and or artist property`;
+}
+console.log(`search({artist:"Usher"})(expect 2 Usher songs in an array)`,search({artist:`Usher`}));
+console.log(`search({artist:"Usher", year:2004})(expect 1 Usher songs in an array)`,search({artist:`Usher`,year:2004}));
+console.log(`search({artist:"Usher", year:2004})(expect 1 Usher songs in an array)`,search({artist:`Usher`,year:2004}));
+console.log(`search({artist:"Usher", year:2004})(expect 1 Usher songs in an array)`,search());
+console.log(`search({artist:"Usher", year:2004})(expect 1 Usher songs in an array)`,search({}));
+console.log
+
+
+// // ex of a pass in value for search: (),{},{artist:`Usher`}, {year: 2020}, and {artist:`Usher`, year: 2020}
+// console.log(`--- Testing search() ---`)
+// console.log(`(expect collection)`,(search('')));
+// console.log(`(expect collection)`,(search({})));
+// console.log(`(expect 1 record from 2012 in array)`,(search({year:2012})));
+// console.log(`(expect 2 record from 2020 in array)`,(search({year:2020})));
+// console.log(`(expect 2 Usher records returned in an array)`,(search({artist:'Usher'})));
+// console.log(`(expect 1 Lana Del Rey record returned in an array)`,(search({artist:'Lana Del Rey'})));
+// console.log(`(expect 1 Nobody record returned in an array)`,(search({artist:'Nobody'})));
+console.log(`(expect 1 record from 2012 in array)`,(search({year:2030})));
+//console.log(`(expect Sam Cooke)`,(search('Change Is Gonna Come')));
